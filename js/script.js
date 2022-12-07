73555a22c3b24b6831c00d161451fd9f5ab6be9f
YoannPitance <pittyo88@gmail.com>
2022-12-07 11:25:43 +0100

    1- yes enfin réussi ma boucle produits

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-function getProducts() {
-  fetch("http://localhost:3000/api/products")
-    .then(function (data) {
-      let products = data;
-      console.log(data);
-    })
+//récupérer les données de l'API
 
-    .catch(function (error) {
-      console.log("Quelque chose ne s'est pas bien passé");
-    });
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((data) => {
+    displayProducts(data);
+  })
+  .catch((error) => {
+    document.querySelector("h2").innerHTML =
+      "<h2>Une erreur s'est produite: erreur 404</h2>";
+  });
+
+//injecter les infos articles dans le html pour afficher les produits sur la page
+function displayProducts(products) {
+  let baliseArticle = document.getElementById("items");
+  for (let product of products) {
+    baliseArticle.innerHTML += `<a href="./products.html?_id=${product._id}">
+      <article>
+        <img src="${product.imageUrl}" alt="${product.altTxt}">
+        <h3 class=productName">${product.name}</h3>
+        <p class=productDescription">${product.description}</p>
+      </article>
+    </a>`;
+  }
 }
-
-getProducts();
