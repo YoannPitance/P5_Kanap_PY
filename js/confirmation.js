0cebc2d1cc34d006d949ecbd9ba248b511aa4c47
YoannPitance <pittyo88@gmail.com>
2023-01-09 09:25:19 +0100

    17- page confirmation ok rest refactoring complet

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,4 +1,12 @@
 //Récupération du numero de comannde dans l url et affichage sur la page confirmation
 const urlOrderId = new URLSearchParams(window.location.search).get("orderId");
-const orderId = document.getElementById("orderId");
-orderId.innerText = urlOrderId;
+if (urlOrderId === null || urlOrderId === "") {
+  alert(
+    "Une erreur s'est produite lors de la validation de votre commande. Veuillez nous en excuser !"
+  );
+  window.location.href = "index.html";
+} else {
+  const orderId = document.getElementById("orderId");
+  orderId.innerText = urlOrderId;
+  console.log(idCommande);
+}
